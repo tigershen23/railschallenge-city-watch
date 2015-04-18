c77e9114fe448eb70f4ada19f13618f3e4e50cde
tigershen23 <tigershen23@gmail.com>
2015-04-18 23:35:43 +0000

    Un-namespace responders controller

--- app/controllers/api/v1/responders_controller.rb
+++
@@ -1,2 +0,0 @@
-class API::V1::RespondersController < ApplicationController
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  namespace :api do
-    namespace :v1, defaults: { format: :json } do
-      resources :responders
-    end
-  end
+  resources :responders, defaults: {format: :json}
 end
