989b5873b9da0182feb4ed0adc0751ef8cb410ef
tigershen23 <tigershen23@gmail.com>
2015-04-19 06:33:02 +0000

    Resolve POST /responders/ lack of *'

--- app/models/responder.rb
+++ app/models/responder.rb
@@ -1,8 +1,9 @@
 class Responder < ActiveRecord::Base
   VALID_CAPACITIES = 1..5
 
-  validates :name, uniqueness: true
+  validates :name, presence: true, uniqueness: true
   validates :capacity, presence: true, inclusion: VALID_CAPACITIES
+  validates :type, presence: true
 
   # Avoid STI; all types of Responders behave the same for now
   self.inheritance_column = nil
