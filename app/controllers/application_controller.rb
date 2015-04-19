eac61d62a01704da7b536f6ec82708ec4350d6e1
tigershen23 <tigershen23@gmail.com>
2015-04-19 03:33:06 +0000

    Passing responders_404_test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::API
+  def not_found
+    error = { message: 'page not found' }.to_json
+    render json: error, status: 404
+  end
 end
--- app/controllers/responders_controller.rb
+++ app/controllers/responders_controller.rb
@@ -1,2 +1,25 @@
 class RespondersController < ApplicationController
+  def create
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def new
+    not_found
+  end
+
+  def edit
+    not_found
+  end
+
+  def destroy
+    not_found
+  end
 end
