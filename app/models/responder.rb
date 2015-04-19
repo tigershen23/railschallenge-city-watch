3d7d57825b252e3d5fd2fb74b886a4cc168fa061
tigershen23 <tigershen23@gmail.com>
2015-04-19 06:21:49 +0000

    Resolve POST /responders/ lack of type returns an error

--- app/models/responder.rb
+++ app/models/responder.rb
@@ -2,7 +2,7 @@ class Responder < ActiveRecord::Base
   VALID_CAPACITIES = 1..5
 
   validates :name, uniqueness: true
-  validates :capacity, inclusion: VALID_CAPACITIES
+  validates :capacity, presence: true, inclusion: VALID_CAPACITIES
 
   # Avoid STI; all types of Responders behave the same for now
   self.inheritance_column = nil
